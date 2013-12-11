df50f78e49f556d269e8cdf0fe510d7d54c347c6
Chris <chris.haack@me.com>
2013-12-11 18:37:21 -0500

    create files

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'bundler'
+require "sinatra/reloader"
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Citibike
+	class App < Sinatra::Application
+    configure :development do
+      register Sinatra::Reloader
+    end
+
+    before do
+      json = File.open("data/citibikenyc.json").read
+      @data = MultiJson.load(json)
+    end
+
+    get '/' do
+      erb :home
+    end
+
+  end
+end
